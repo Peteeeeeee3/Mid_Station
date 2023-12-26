8bf004ad0a53da022eb7bba1c6f169965546efcd
Xavier Murrow <xavmurrow@hotmail.co.uk>
2023-12-26 17:30:26 +0000

    Flask Project with initial website

---
+++ Code/Website_UI/flaskProject/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template as rt
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():  # put application's code here
+    return rt('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
