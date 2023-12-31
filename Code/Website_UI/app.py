9287aa861808c026948fd24432b0e12986456ae0
Peteeeeeee3 <peter.farkas@city.ac.uk>
2023-12-31 02:14:09 +0000

    creating new setting and toggling active setting works, editing setting in progress

--- Code/Website_UI/app.py
+++ Code/Website_UI/app.py
@@ -6,31 +6,64 @@ app = Flask(__name__)
 client = pymongo.MongoClient("mongodb+srv://PeterFarkas:tEiOltcXCGih8y7U@midstationdb0.8jpo7hd.mongodb.net/?retryWrites=true&w=majority")
 db = client['mid-station']
 user = dict(db.User.find_one({"email": "a.b@gmail.com"}, {}))
+email = user['email']
 
 @app.route('/')
 @app.route('/home')
-def home():  
-    #return rt('home.html')
+def home():
     return rt('stream.html', settings=enumerate(user['streamSetting']))
 
-@app.route('/stream', methods=['POST', 'GET'])
-def stream_page():  
-    if request.method == 'POST':
-        for idx, status in request.form.items():
-            user['streamSetting'][int(idx)]['active'] = True if status=='on' else False
+@app.route('/stream', defaults={'settingIdx': None})
+@app.route('/stream/<int:settingIdx>')
+def stream_page(settingIdx=None):
+    # get users settings
+    user = dict(db.User.find_one({"email": email}, {}))
+
+    # user trying to change current setting
+    if settingIdx is not None:
+
+        # resolve new settings (active)
+        for idx, setting in enumerate(user['streamSetting']):
+            if idx != settingIdx:
+                setting['active'] = False
+            else:
+                if setting['active']:
+                    setting['active'] = False
+                else:
+                    setting['active'] = True
+
+        # update database
+        db.User.update_one( { "email": email }, {"$set": {"streamSetting": user['streamSetting']}})
     return rt('stream.html', settings=enumerate(user['streamSetting']))
 
-# @app.route('/set-active-stream', methods=['POST'])
-# def setActiveStream():
-#     if request.method == 'POST':
-#         # middleware logic -> session active
+@app.route('/new-setting', methods=["GET", "POST"])
+def createNewSetting():
 
+    # get user
+    user = dict(db.User.find_one({"email": email}, {}))
+    
+    if request.method == "POST":
+        title = request.form['stream-title']
+        numSettings = len(user['streamSetting'])
+        user['streamSetting'].insert(numSettings, {'name': title, 'active': False})
+        db.User.update_one( {"email": email}, {"$set": {"streamSetting": user['streamSetting']}})
+    return rt('stream.html', settings=enumerate(user['streamSetting']))
 
-#         # redirect
-#         print(request.form['peter'])
-#         return 'hello'
-#     else:
-#         return 'fuckoff' 
+@app.route('/edit-setting', methods=["GET", "POST"])
+def editSetting():
+    # get user
+    user = dict(db.User.find_one({"email": email}, {}))
+
+    if request.method == "POST":
+        old_title = # add name of correct setting here
+        title = request.form['stream-title']
+        for setting in user['streamSetting']:
+            if setting['name'] == old_title:
+                setting['name'] = title
+        
+        db.User.update_one( {"email": email}, {"$set": {"streamSetting": user['streamSetting']}})
+    
+    return rt('stream.html', settings=enumerate(user['streamSetting']))
 
 
 if __name__ == '__main__':
